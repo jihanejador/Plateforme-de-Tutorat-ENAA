a30278725960322707ccf197008f487d8873b843
jihanejador <jihanejador@gmail.com>
2026-05-21 10:10:10 +0100

    faire le getter et setter de class helprequest

--- Entities/HelpRequest.php
+++ Entities/HelpRequest.php
@@ -21,4 +21,24 @@ class HelpRequest {
         $this->tuteurId = $tutor->getId();
         $this->statut = Statut::ASSIGNED;
     }
+    public function getId(): ?int{ return $this->id; }
+    public function setId(int $id): void { $this->id = $id; }
+
+    public function getTitre(): string { return $this->titre; }
+    public function setTitre(string $titre): void { $this->titre = $titre; }
+
+    public function getDescription(): string { return $this->description; }
+    public function setDescription(string $desc): void { $this->description = $desc; }
+
+    public function getTechnologie(): string { return $this->technologie; }
+    public function setTechnologie(string $tech): void { $this->technologie = $tech; }
+
+    public function getStatut(): Statut { return $this->statut; }
+    public function setStatut(Statut $statut): void { $this->statut = $statut; }
+
+    public function getApprenantId(): int { return $this->apprenantId; }
+    public function setApprenantId(int $id): void { $this->apprenantId = $id; }
+
+    public function getTuteurId(): ?int { return $this->tuteurId; }
+    public function setTuteurId(?int $id): void { $this->tuteurId = $id; }
 }
