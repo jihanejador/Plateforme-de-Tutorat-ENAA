9bd7178e8024a5b541ed64cb664857866b392b34
jihanejador <jihanejador@gmail.com>
2026-05-21 10:43:58 +0100

    return les objet raw

--- Repositories/UserRepository.php
+++ Repositories/UserRepository.php
@@ -14,5 +14,17 @@ class UserRepository {
     public function findByEmail(string $email): ?User {
         $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email = :email");
         $stmt->execute(['email'=> $email]);
+
+        $row = $stmt->fetch(PDO::FETCH_OBJ);
+        if(!$row) return null;
+
+        $user = new User();
+        $user->setId((int)$row->id);
+        $user->setNom($row->nom);
+        $user->setEmail($row->email);
+        $user->setRole($row->role);
+        $user->setCompetencesMaitrisees($row->competences_maitrisees);
+        $user->setCompetencesATravailler($row->competences_a_travailler);
+        return $user;
     }
 }
