355d1cdabd2e452a65188f9e715bfc1faada20da
jihanejador <jihanejador@gmail.com>
2026-05-21 10:24:50 +0100

    craetion le class et les attrubes de Review

---
+++ Entities/Review.php
@@ -0,0 +1,13 @@
+<?php
+namespace Entities;
+
+use Exception; 
+
+class Review{
+    private ?int $id = null;
+    private int $note;
+    private ?string $commentaire = null;
+    private int $helpRequestId;
+
+    
+}
