65ca5224f6d4ac691d4ef9a30683dd6275dfe26d
jihanejador <jihanejador@gmail.com>
2026-05-21 10:00:51 +0100

    validation de l assignation

--- Entities/HelpRequest.php
+++ Entities/HelpRequest.php
@@ -13,4 +13,12 @@ class HelpRequest {
     private string $dateCreation;
     private int $apprenantId;
     private ?int $tuteurId = null;
+
+    public function assignTo(User $tutor): void{
+        if($tutor->getId() === $this->apprenantId){
+            throw new Exception("Un tuteur ne peut pas s assigner sa propre demande d aide.");
+        }
+        $this->tuteurId = $tutor->getId();
+        $this->statut = Statut::ASSIGNED;
+    }
 }
