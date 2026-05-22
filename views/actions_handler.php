f7767a8e8e89f9ac3be47914e96465fe5aadd92f
jihanejador <jihanejador@gmail.com>
2026-05-22 10:04:44 +0100

    Créer une demande d'aide

--- views/actions_handler.php
+++ views/actions_handler.php
@@ -16,3 +16,20 @@ $userRepo = new \Repositories\UserRepository($database->pdo);
 if(!isset($_SESSION['user_id'])){
     $_SESSION['user_id'] = 1;
 }
+
+$action = $_GET['action'] ?? '';
+
+try{
+    if($action === 'create' && $_SERVER['REQUEST_METHOD'] === 'POST'){
+        $ticket = new \Entities\HelpRequest();
+        $ticket->setTitre(string: $_POST['titre']);
+        $ticket->setDescription(string: $_POST['description']);
+        $ticket->setTechnologie(string: $_POST['technologie']);
+        $ticket->setStatut(\Enums\Statut::PENDING);
+        $ticket->setApprenantId((int)$_SESSION['user_id']);
+
+        $ticketRepo->save($ticket);
+        header('Location: dashboard.php?success=ticket_created');
+        exit();
+    }
+}
