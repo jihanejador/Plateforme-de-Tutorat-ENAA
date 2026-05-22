218912b36c65a9f13ebdf96bd75c0e0649a2f7b4
jihanejador <jihanejador@gmail.com>
2026-05-22 10:43:15 +0100

    Récupérer les tickets en attente

---
+++ views/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../Enums/Status.php';
+require_once __DIR__ . '/../Entities/HelpRequest.php';
+require_once __DIR__ . '/../Repositories/TicketRepository.php';
+
+$database = new Databasr();
+$ticketRepo = new \Repositories\TicketRepository($database->pdo);
+
+$ticket = $ticketRepo->findAllPending();
